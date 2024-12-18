e201208132e8a84691d55f581ee399aba5501766
Timur <timur.tolibov04@gmail.com>
2024-12-18 20:01:23 +0500

    This file for lesson

---
+++ script.js
@@ -0,0 +1,45 @@
+let num1 = 1;
+for (let i = 1; i <= 10; ++i)
+    console.log(num1 + " * " + i +
+        " = " + num1 * i);
+
+        let num2 = 2;
+for (let i = 1; i <= 10; ++i)
+    console.log(num2 + " * " + i +
+        " = " + num2 * i);
+
+        let num3 = 3;
+for (let i = 1; i <= 10; ++i)
+    console.log(num3 + " * " + i +
+        " = " + num3 * i);
+
+        let num4 = 4;
+for (let i = 1; i <= 10; ++i)
+    console.log(num4 + " * " + i +
+        " = " + num4 * i);
+
+        let num5 = 5;
+for (let i = 1; i <= 10; ++i)
+    console.log(num5 + " * " + i +
+        " = " + num5 * i);
+
+        let num6 = 6;
+for (let i = 1; i <= 10; ++i)
+    console.log(num6 + " * " + i +
+        " = " + num6 * i);
+
+        let num7 = 7;
+for (let i = 1; i <= 10; ++i)
+    console.log(num7 + " * " + i +
+        " = " + num7 * i);
+
+        let num8 = 8;
+for (let i = 1; i <= 10; ++i)
+    console.log(num8 + " * " + i +
+        " = " + num8 * i);
+
+
+let num9 = 9;
+for (let i = 1; i <= 10; ++i)
+    console.log(num9 + " * " + i +
+        " = " + num9 * i);
